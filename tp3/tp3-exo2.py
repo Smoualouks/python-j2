41aff9e5a5e7ef44b87a852b2fa28fd5ae3d59c1
root <omari.ismail@gmail.com>
2023-01-01 12:09:17 +0100

    tp3 exo2 ended.

--- tp3/tp3-exo2.py
+++ tp3/tp3-exo2.py
@@ -1,29 +1,47 @@
 import csv
 
-# Ouvrir le fichier deniro.csv
+# j'initalise la liste scores qui contiendera tous les scores, histoire d'avoir un itérable
+# j'initialise la liste filmsName qui contiendera tous les noms des films
+# j'initialise la liste years qui contiendera toutes les années de sortie de filsm
+scores = []
+filmsName = []
+years = []
+filmsCount = 0
 
-bestScore = -1
-nombeFilms = 0
+# j'ouvre le fichier deniro.csv:
 
 with open('deniro.csv', 'r') as f:
 
-       # Créer un objet csv à partir du fichier
+    # je me lis le contenu du fichier csv.
+    
+    myReader = csv.reader(f)
+    # j'invoque la méthode next() pour sauter la première ligne contenant le nom des colonnes.
+    next(myReader)
+    
+    # j'appele la méthode append() pour alimenter toutes mes listes précedement initialisées.
+    for line in myReader:
 
-       myReader = f.readlines()
+       years.append(int(line[0]))
+       scores.append(int(line[1]))
+       filmsName.append(line[2])
 
-       # je suprime la première ligne contenant les noms des colonnes:
-       myReaderWithoutFirstLine = myReader[1:]
-      
-       #for line in myReaderWithoutFirstLine:
-       
 
-        
-        #  for i in line:
-        #     if i[1] > bestScore:
-        #         bestScore = i[1]
-        #     if i[0] >= 2000 and i[0] <= 2010:
-        #         nombeFilms += 1
-        
+# je récuprè l'index de film ayant le score max
+index = scores.index(max(scores))
+
+# je counte le nombre de films sorti entre [2000,2010]          
+for year in years:
+       if year >= 2000 and year <= 2010:
+              filmsCount += 1
+
+
+# je crée un nouveau fichoer txt ou je récupère le nom du film ayant le meilleur score et 
+#le nombre de films sorti entre 2000 et 2010
+filepath = "deniro-report.txt"
+f = open(filepath, "w")
+f.write("Nom du film le mieux noté est: " + filmsName[index][2:-2] + "\n" + "Nombre de films entre 2000 et 2010 est: " + str(filmsCount))
+f.close()       
+#-------------------------------------------- The End ----------------------------------------------------------       
              
             
            
