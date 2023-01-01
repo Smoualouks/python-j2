101ae6a62349c22c24123ba6ca3534200cbf9525
root <omari.ismail@gmail.com>
2023-01-01 12:14:44 +0100

    Adjusting Orthographe.

--- tp3/tp3-exo2.py
+++ tp3/tp3-exo2.py
@@ -3,6 +3,8 @@ import csv
 # j'initalise la liste scores qui contiendera tous les scores, histoire d'avoir un itérable
 # j'initialise la liste filmsName qui contiendera tous les noms des films
 # j'initialise la liste years qui contiendera toutes les années de sortie de filsm
+# la variable filmsCount, pour compter le nombre des films entre 2000 et 2010
+
 scores = []
 filmsName = []
 years = []
@@ -12,7 +14,7 @@ filmsCount = 0
 
 with open('deniro.csv', 'r') as f:
 
-    # je me lis le contenu du fichier csv.
+    # je lis le contenu du fichier csv.
     
     myReader = csv.reader(f)
     # j'invoque la méthode next() pour sauter la première ligne contenant le nom des colonnes.
@@ -26,21 +28,23 @@ with open('deniro.csv', 'r') as f:
        filmsName.append(line[2])
 
 
-# je récuprè l'index de film ayant le score max
+# je récupère l'index du film ayant le score max
 index = scores.index(max(scores))
 
-# je counte le nombre de films sorti entre [2000,2010]          
+# je compte le nombre des films sorti entre [2000,2010]          
 for year in years:
        if year >= 2000 and year <= 2010:
               filmsCount += 1
 
 
 # je crée un nouveau fichoer txt ou je récupère le nom du film ayant le meilleur score et 
-#le nombre de films sorti entre 2000 et 2010
+#le nombre des films sortis entre 2000 et 2010
 filepath = "deniro-report.txt"
 f = open(filepath, "w")
 f.write("Nom du film le mieux noté est: " + filmsName[index][2:-2] + "\n" + "Nombre de films entre 2000 et 2010 est: " + str(filmsCount))
 f.close()       
+
+
 #-------------------------------------------- The End ----------------------------------------------------------       
              
             
